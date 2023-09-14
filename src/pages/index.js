b4242ec8e7ef1a3f16c92567377c340eab36767c
Nofrialdi <aldi.fasive@gmail.com>
2023-09-14 17:08:22 +0700

    feat(view image and name pokemon): view all list pokemon and name in home page
    view all list pokemon and name in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,30 @@ import { useGlobalContext } from "@/context/global";
 
 export default function Home() {
 	const { allPokemonData } = useGlobalContext();
-	console.log(allPokemonData);
-	return <main></main>;
+	// console.log(allPokemonData);
+	return (
+		<main>
+			<div className="all-pokemon">
+				{allPokemonData ? (
+					allPokemonData.map((pokemon) => {
+						return (
+							<div key={pokemon.id} className="pokemon">
+								<div className="card-image">
+									<img src={pokemon.sprites.other.home.front_shiny} alt={pokemon.name} />
+								</div>
+								<div className="card-body">
+									<h1>{pokemon.name}</h1>
+								</div>
+								<div>
+									<p>More Detail &nbsp;&rarr;</p>
+								</div>
+							</div>
+						);
+					})
+				) : (
+					<h1>Loading...</h1>
+				)}
+			</div>
+		</main>
+	);
 }
